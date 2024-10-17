468e939f43f7261a9e77fc467161e0fe986855cd
2022111770 <2609834220@qq.com>
2024-10-17 11:18:53 +0800

    LoadMq

--- src/main/java/com/magq/mq/server/LoadMq.java
+++ src/main/java/com/magq/mq/server/LoadMq.java
@@ -3,7 +3,21 @@ package com.magq.mq.server;
 public class LoadMq {
 
     public static void main(String[] args) {
-        BrokerServer brokerServer = new BrokerServer();
-        brokerServer.start();
+        // 启动 Broker 服务器
+        Thread brokerThread = new Thread(() -> {
+            BrokerServer brokerServer = new BrokerServer();
+            brokerServer.start();
+        });
+
+        brokerThread.start();
+
+        // 等待 Broker 服务器启动完成
+        try {
+            Thread.sleep(1000); // 等待 1 秒钟确保 Broker 启动
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Broker server has started.");
     }
 }
